28e55d57e808d027bf6969a5a8d569301ab09e7b
Silas Oyieyo <you@example.com>
2023-07-12 12:46:03 +0200

    simple project code and httml

--- Water_managementproject_coding.R
+++ Water_managementproject_coding.R
@@ -139,6 +139,7 @@ plot_distributions(mcSimulation_object = dripline_mc_simulation,
 
 #### SURFACE IRRIGATION ####
 #surface irrigation pathway
+library(igraph)
 SurfaceIrr_path<-graph.formula(SurfaceIrr-+Yield,
                                SurfaceIrr-+Incomes,
                                SurfaceIrr-+Management,
@@ -149,7 +150,7 @@ SurfaceIrr_path<-graph.formula(SurfaceIrr-+Yield,
                                Yields-+TotalBenefits,
                                TotalBenefits-+CurrentSituationOutcome,
                                TotalCost-+CurrentSituationOutcome)
-#plot(SurfaceIrr_path)
+plot(SurfaceIrr_path)
 
 SurfaceIrr_Path<-graph.formula()
 
@@ -188,95 +189,5 @@ plot_distributions(mcSimulation_object = SurfaceIrr_mc_simulation,
                    new_names = "Outcome distribution for profits")
 
 ###########################################################################
-# Imports ####
-library(readr)
-library(decisionSupport)
-
-# Estimates table
-input_table<-read.csv("Estimates.csv")
-#View(input_table)
-
-# Model function ####
-irrigation_model_function<-function(x, varnames){
-  
-  # calculate drought risks: impact the implementation of drought ####
-  droughtEvent <-chance_event(Drought_Event, 1, 0, n = 1)
-  
-  
-  #  Intervention ####
-  for (decision_drip_irrigation in c(FALSE,TRUE)){
-    
-    if (decision_drip_irrigation){
-      
-      # Profits ####
-      Profits<-vv(Drip_Yield,Var_CV,n_years)*Marketvalue+Drip_Management+Drip_All_other_incomes
-      
-      # Costs ####
-      Overallcosts<- Drip_Establishmentcost
-      
-      # Results ####
-      net_benefits <- Profits - Overallcosts
-      result_drip <- net_benefits
-    }
-    
-    else{
-      
-      # Profits ####
-      # Drought
-      if (droughtEvent){
-        Profits<-vv(Surface_Yield,Var_CV,n_years)*
-          (1-vv(Drought_Discount, Var_CV,n_years))*
-          Marketvalue+Surface_Management+Surface_All_other_incomes
-      }
-      
-      # No drought
-      else{
-        Profits<-vv(Surface_Yield,Var_CV,n_years)*Marketvalue
-        +Surface_Management+Surface_All_other_incomes}
-      
-      # Costs ####
-      Overallcosts<- Surface_MaintenanceCost
-      
-      # Results ####
-      net_benefits <- Profits - Overallcosts
-      result_surface <- net_benefits}
-    
-  }   #close intervention loop bracket
-  NPV_interv <-
-    discount(result_drip, discount_rate, calculate_NPV = TRUE)
-  
-  NPV_n_interv <-
-    discount(result_surface, discount_rate, calculate_NPV = TRUE)
-  
-  return(list(Drip_NPV = NPV_interv,
-              Surf_NPV = NPV_n_interv,
-              NPV_decision_do = NPV_interv - NPV_n_interv,
-              Cashflow_decision_do = result_drip - result_surface))}
-
-mcSimulation_results <- decisionSupport::mcSimulation(
-  estimate = decisionSupport::estimate_read_csv("Estimates.csv"),
-  model_function = irrigation_model_function,
-  numberOfModelRuns = 200,
-  functionSyntax = "plainNames"
-)
-#par(mfrow = c(1, 1))
-# Plot distrbution ####
-decisionSupport::plot_distributions(mcSimulation_object = mcSimulation_results, 
-                                    vars = c("Drip_NPV","Surf_NPV"),
-                                    method ='smooth_simple_overlay', 
-                                    base_size = 7)+
-decisionSupport::plot_distributions(mcSimulation_object = mcSimulation_results, 
-                                      vars = c("Drip_NPV",
-                                               "Surf_NPV"),
-                                      method = 'boxplot')
-
-
-#######################################################
-#plot_distributions(mcSimulation_object = mcSimulation_results,
-                   #vars = c("Drip_NPV","Surf_NPV"),
-                   #method = "boxplot_density",
-                   #old_names = "final_result",
-                   #base_size = 7,
-                   #new_names = "Outcome distribution for profits")
 
 
